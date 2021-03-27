8ec71b2d644aa4479acddd303f7f2ce63e99462c
Lenard Lindstrom <len-l@telus.net>
2021-03-27 14:45:30 -0700

    Mangle names to avoid name conflicts
    External names imported into the local namespace have __{n} suffix added to
    the name. Other names line 's' and 'd' are replaced with _{n} suffix
    names. Also, use names slightly more descriptive than single letter
    identifiers.

--- blitkit.py
+++ blitkit.py
@@ -13,26 +13,31 @@ class Blitter:
 class BlitterFactory:
     """Base class"""
 
-    def __init__(self, src_type, dst_type, loop_indices):
-        self.src_type = src_type
-        self.dst_type = dst_type
+    def __init__(self, arg1_type, arg2_type, loop_indices):
+        self.arg1_type = arg1_type
+        self.arg2_type = arg2_type
         self.loop_indices = loop_indices
 
     def __call__(self, fn):
         fn_name = name_of(fn)
-        tree = self.make_tree(fn_name)
+        mangled_fn_name = f"{fn_name}__0"
+        type_name_1 = name_of(self.arg1_type)
+        mangled_type_name_1 = f"{type_name_1}__1"
+        type_name_2 = name_of(self.arg2_type)
+        mangled_type_name_2 = f"{type_name_2}__2"
+        mangled_type_names = [mangled_type_name_1, mangled_type_name_2]
+        tree = self.make_tree(fn_name, mangled_fn_name, mangled_type_names)
         code = compile(tree, '<C_Iterator>', 'exec')
-        gbls = {'src_type': self.src_type, 'dst_type': self.dst_type,
-                f'{fn_name}_0': fn}
+        gbls = {f'{mangled_type_name_1}': self.arg1_type,
+                f'{mangled_type_name_2}': self.arg2_type,
+                f'{mangled_fn_name}': fn}
         lcls = {}
         exec(code, gbls, lcls)
         blit = lcls[fn_name]
-        st_name = name_of(self.src_type)
-        dt_name = name_of(self.dst_type)
         trav = ", ".join(str(i) for i in self.loop_indices)
         blit.__doc__ = (
-            f"{fn_name}(s: {st_name}, d: {dt_name}) -> None\n\n"
-             "Blit s to d. This version uses C pointer arithmetic only\n"
+            f"{fn_name}(src: {type_name_1}, dst: {type_name_2}) -> None\n\n"
+             "Blit src to dst. This version uses C pointer arithmetic only\n"
             f"to traverse over elements in index order [{trav}].")
         blit.tree = tree
         blit.wraps = fn
@@ -42,7 +47,7 @@ class BlitterFactory:
         raise NotImplementedError("Abstract base function")
 
 class C_Iterators(BlitterFactory):
-    def make_tree(self, fn_name):
+    def make_tree(self, fn_name, wrapped_fn_name, type_names):
         loop_indices = self.loop_indices
         ndims = len(loop_indices)
 
@@ -52,93 +57,93 @@ class C_Iterators(BlitterFactory):
         b = astkit.TreeBuilder()
         b.identifier(fn_name)
         b.arguments()
-        b.identifier('s')
-        b.identifier('d')
+        b.identifier('arg_1')
+        b.identifier('arg_2')
         b.end()
         b.FunctionDef()
 
         # Array dimensions and starting points
-        get_dims(b, ndims, 'src_type', 's')
-        b.Name('sp_0')
-        b.Name('src_type')
+        get_dims(b, ndims, type_names[0], 'arg_1')
+        b.Name('parg_1')
+        b.Name(type_names[0])
         b.identifier('pointer')
         b.Attribute()
         b.Call()
-        b.Name('s')
+        b.Name('arg_1')
         b.end()
         b.Assign1()
-        b.Name('dp_0')
-        b.Name('dst_type')
+        b.Name('parg_2')
+        b.Name(type_names[1])
         b.identifier('pointer')
         b.Attribute()
         b.Call()
-        b.Name('d')
+        b.Name('arg_2')
         b.end()
         b.Assign1()
 
         # Pointer increments
-        get_strides(b, ndims, 'src_type', 's')
-        get_strides(b, ndims, 'dst_type', 'd')
-        get_delta(b, loop_indices[0], loop_indices[1], 's')
-        get_delta(b, loop_indices[0], loop_indices[1], 'd')
+        get_strides(b, ndims, type_names[0], 'arg_1')
+        get_strides(b, ndims, type_names[1], 'arg_2')
+        get_delta(b, loop_indices[0], loop_indices[1], 'arg_1')
+        get_delta(b, loop_indices[0], loop_indices[1], 'arg_2')
 
         # Loop over outer index
         i = loop_indices[0]
-        b.Name(f's_end_{i}')
-        b.Name('sp_0')
-        b.Name(f's_stride_{i}')
-        b.Name(f'd_{i}')
+        b.Name(f'arg_1_end_{i}')
+        b.Name('parg_1')
+        b.Name(f'arg_1_stride_{i}')
+        b.Name(f'dim_{i}')
         b.Mult()
         b.Add()
         b.Assign1()
-        b.Name('sp_0')
-        b.Name(f's_end_{i}')
+        b.Name('parg_1')
+        b.Name(f'arg_1_end_{i}')
         b.Lt()
         b.While()
 
         # Loop over inner index
         i = loop_indices[1]
-        b.Name(f's_end_{i}')
-        b.Name('sp_0')
-        b.Name(f's_stride_{i}')
-        b.Name(f'd_{i}')
+        b.Name(f'arg_1_end_{i}')
+        b.Name('parg_1')
+        b.Name(f'arg_1_stride_{i}')
+        b.Name(f'dim_{i}')
         b.Mult()
         b.Add()
         b.Assign1()
-        b.Name('sp_0')
-        b.Name(f's_end_{i}')
+        b.Name('parg_1')
+        b.Name(f'arg_1_end_{i}')
         b.Lt()
         b.While()
-        b.Name(f'{fn_name}_0')
+        b.Name(wrapped_fn_name)
         b.Call()
-        b.Name('src_type')
+        b.Name(type_names[0])
         b.identifier('Pixel')
         b.Attribute()
         b.Call()
-        b.Name('sp_0')
+        b.Name('parg_1')
         b.end()
-        b.Name('dst_type')
+        b.Name(type_names[1])
         b.identifier('Pixel')
         b.Attribute()
         b.Call()
-        b.Name('dp_0')
+        b.Name('parg_2')
         b.end()
         b.end()
         b.Expr()
-        b.Name('sp_0')
-        b.Name(f's_stride_{i}')
+        b.Name('parg_1')
+        b.Name(f'arg_1_stride_{i}')
         b.IAdd()
-        b.Name('dp_0')
-        b.Name(f'd_stride_{i}')
+        b.Name('parg_2')
+        b.Name(f'arg_2_stride_{i}')
         b.IAdd()
         b.end()  # inner loop
 
         i = loop_indices[0]
-        b.Name('sp_0')
-        b.Name(f's_delta_{i}')
+        b.Name('parg_1')
+        b.Name(f'arg_1_delta_{i}')
         b.IAdd()
-        b.Name('dp_0')
-        b.Name(f'd_delta_{i}')
+        b.Name('parg_2')
+        b.Name(f'arg_2_delta_{i}')
         b.IAdd()
         b.end()  # outer loop
 
@@ -148,7 +153,7 @@ class C_Iterators(BlitterFactory):
 def get_dims(build, ndims, typ, name):
     build.Tuple()
     for i in range(ndims):
-        build.Name(f'd_{i}')
+        build.Name(f'dim_{i}')
     build.end()
     build.Name(typ)
     build.identifier('size_of')
@@ -175,7 +180,7 @@ def get_delta(build, index, prev_index, name):
     build.Name(f'{name}_delta_{index}')
     build.Name(f'{name}_stride_{index}')
     build.Name(f'{name}_stride_{prev_index}')
-    build.Name(f'd_{prev_index}')
+    build.Name(f'dim_{prev_index}')
     build.Mult()
     build.Sub()
     build.Assign1()
@@ -197,35 +202,36 @@ def name_of(o):
 #     def do_blit(s, d):
 #         pass
 #     
-# Function globals are src_type, dst_type and fn_0.
+# Function globals are:
+#     {'Array__1': Array, 'Surface__2': Surface, 'do_blit__0': do_blit}
 #
-def do_blit(s, d):
-    """do_blit(s: Array, d: Surface) -> None
+def do_blit(arg_1, arg_2):
+    """do_blit(src: Array, dst: Surface) -> None
 
-Blit s to d. This version uses C pointer arithmetic only
+Blit src to dst. This version uses C pointer arithmetic only
 to traverse over elements in index order [1, 0]."""
     # Array dimensions and starting points
-    d_0, d_1 = src_type.size_of(s)
-    sp_0 = src_type.pointer(s)
-    dp_0 = dst_type.pointer(d)
+    dim_0, dim_1 = Array_1.size_of(arg_1)
+    parg_1 = Array__1.pointer(arg_1)
+    parg_2 = Surface__2.pointer(arg_2)
 
     # Pointer increments
-    s_stride_0, s_stride_1 = src_type.strides(s)
-    d_stride_0, d_stride_1 = dst_type.strides(d)
-    s_delta_1 = s_stride_1 - s_stride_0 * d_0
-    d_delta_1 = d_stride_1 - d_stride_0 * d_0
+    arg_1_stride_0, arg_1_stride_1 = Array__1.strides(a_1)
+    arg_2_stride_0, arg_2_stride_1 = Surface__2.strides(a_2)
+    arg_1_delta_1 = arg_1_stride_1 - arg_1_stride_0 * dim_0
+    arg_2_delta_1 = arg_2_stride_1 - arg_2_stride_0 * dim_0
 
     # Loop over index 1...
-    s_end_1 = sp_0 + s_stride_1 * d_1
-    while sp_0 < s_end_1:
+    arg_1_end_1 = parg_1 + arg_1_stride_1 * dim_1
+    while parg_1 < arg_1_end_1:
         # Loop over index 0...
-        s_end_0 = sp_0 + s_stride_0 * d_0
-        while sp_0 < s_end_0:
-            do_blit_0(src_type.Pixel(sp_0), dst_type.Pixel(dp_0))
-            sp_0 += s_stride_0
-            dp_0 += d_stride_0
+        arg_1_end_0 = parg_1 + arg_1_stride_0 * dim_0
+        while parg_1 < arg_1_end_0:
+            do_blit__0(Array__1.Pixel(parg_1), Surface__2.Pixel(parg_2))
+            parg_1 += arg_1_stride_0
+            parg_2 += arg_2_stride_0
 
-        sp_0 += s_delta_1
-        dp_0 += d_delta_1
+        parg_1 += arg_1_delta_1
+        parg_2 += arg_2_delta_1
 
 blitter = Blitter(C_Iterators, [1, 0])

ca139701673c6a8e5fdc2bb3c9f83a4ba74b9ec4
Lenard Lindstrom <len-l@telus.net>
2021-04-15 11:42:44 -0700

    Exploring Code Analysis: Flow Control Graphs
    A test program, and its test data, to generate a flow control graph.

---
+++ fcg.py
@@ -0,0 +1,72 @@
+import ast
+
+class FlowControlMapper(ast.NodeVisitor):
+    """Create a flow control graph"""
+
+    def __init__(self):
+        self.graphs = {}
+        self.graph = None
+
+    def visit_FunctionDef(self, node):
+        self.block = []
+        self.graph = {0: self.block}
+        self.next_label = 1
+        for stmt in node.body:
+            self.visit(stmt)
+        self.graphs[node.name] = self.graph
+        del self.next_label
+        del self.block
+        self.graph = None
+
+    def visit_Assign(self, node):
+        self.block.append(node)
+
+    def visit_AugAssign(self, node):
+        self.block.append(node)
+
+    def visit_Expr(self, node):
+        self.block.append(node)
+
+    def visit_While(self, node):
+        next_block_label = self.next_label
+        self.next_label += 1
+        while_label = self.next_label
+        self.next_label += 1
+        self.block.append(while_label)
+        self.block = []
+        self.graph[while_label] = self.block
+        self.block.append((node.test, self.next_label))
+        self.block.append(next_block_label)
+        self.block = []
+        self.graph[self.next_label] = self.block
+        self.next_label += 1
+        for stmt in node.body:
+            self.visit(stmt)
+        self.block.append(while_label)
+        self.block = []
+        self.graph[next_block_label] = self.block
+
+def pprint(graph, indent=0):
+    for label, block in graph.items():
+        print(f"L{label:<2}: ", end='')
+        for stmt in block:
+            if isinstance(stmt, ast.AST):
+                print(ast.unparse(stmt))
+            elif isinstance(stmt, tuple):
+                print(f"IF {ast.unparse(stmt[0])} GOTO L{stmt[1]}")
+            else:
+                print(f"GOTO L{stmt}")
+            print("     ", end='')
+        print()
+
+def test():
+    with open('test_blitter.py', 'r', encoding='utf-8') as f:
+        module_ast = ast.parse(f.read(), 'test_blitter.py', 'exec')
+    mapper = FlowControlMapper()
+    mapper.visit(module_ast)
+    for name, graph in mapper.graphs.items():
+        print(f"{name}()")
+        pprint(graph)
+
+if __name__ == '__main__':
+    test()

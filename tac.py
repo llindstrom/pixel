b921cda15b009590a42900d21ee9dfc5db5747e2
Lenard Lindstrom <len-l@telus.net>
2021-04-15 22:24:25 -0700

    Determine useless names of a basic block
    This algorithm does not make much sense. Maybe a mistake in the code.

---
+++ tac.py
@@ -0,0 +1,9 @@
+def tac():
+    t1 = 4 - 2
+    t2 = t1
+    t3 = a * t2
+    t4 = t3 * t1
+    t5 = t4 + b
+    t6 = t3 * t1
+    t7 = t6 + b
+    c = t5 * t7
---
+++ useless1.py
@@ -0,0 +1,103 @@
+"""Determine useless names of a basic block
+
+https://www.csd.uwo.ca/~mmorenom/CS447/Lectures/CodeOptimization.html/
+node4.html#algoUuselessNamesOfABasicBlock
+"""
+
+import fcg
+import sys
+import ast
+
+def vars(node):
+    return set(n.id for n in ast.walk(node) if isinstance(n, ast.Name))
+
+def find(A, E, graph, V):
+    L = set()
+    if not E:
+        return L
+    E2 = set()
+    for label in E:
+        if label in V:
+            continue
+        block = graph[label]
+        for stmt in block:
+            if isinstance(stmt, tuple):
+                L |= A & vars(stmt[0])
+                E2.add(stmt[1])
+            elif isinstance(stmt, ast.AST):
+                L |= A & vars(stmt)
+            else:
+                E2.add(stmt)
+        V.add(label)
+    return L | find(A, E2, graph, V)
+
+def lives(label, graph):
+    A = set()  # All variables in the block
+    E = set()  # Graph edges leaving the block as a set of labels`
+    block = graph[label]
+    V = set([label])  # Blocks already visited
+
+    # Collect A and E for the block.
+    for stmt in block:
+        if isinstance(stmt, tuple):
+            A |= vars(stmt[0])
+            E.add(stmt[1])
+        elif isinstance(stmt, ast.AST):
+            A |= vars(stmt)
+        else:
+            E.add(stmt)
+    
+    # Build L from elements in A found in other blocks.
+    return find(A, E, graph, V)
+
+def useless(label, graph):
+    block = graph[label]
+    D = set()
+    L = lives(label, graph)
+    for stmt in block[-1::-1]:
+        if isinstance(stmt, ast.Assign):
+            loads = {node.id for node in ast.walk(stmt.value)
+                     if isinstance(node, ast.Name)}
+            for target in stmt.targets:
+                if isinstance(target, ast.Name):
+                    if target.id not in L:
+                        D.add(target.id)
+                    else:
+                        L.update(loads)
+                elif isinstance(target, ast.Tuple):
+                    for node in target.elts:
+                        if isinstance(node, ast.Name):
+                            if node.id not in L:
+                                D.add(node.id)
+                            else:
+                                L.update(loads)
+        elif isinstance(stmt, ast.AugAssign):
+            loads = {node.id for node in ast.walk(stmt.value)
+                     if isinstance(node, ast.Name)}
+            target = stmt.target
+            if isinstance(target, ast.Name):
+                if target.id not in L:
+                    D.add(target.id)
+                else:
+                    L.add(target.id)
+                    L.update(loads)
+        # What about attribute and index assignment?
+    return D - L
+
+def test():
+    assert(len(sys.argv) == 2)
+    with open(sys.argv[1], 'r', encoding='utf-8') as f:
+        module_ast = ast.parse(f.read(), 'test_blitter.py', 'exec')
+    mapper = fcg.FlowControlMapper()
+    mapper.visit(module_ast)
+    graphs = mapper.graphs
+    for fname, graph in graphs.items():
+        print(f"{fname}()")
+        fcg.pprint(graph)
+        print()
+        for label in graph.keys():
+            U = useless(label, graph)
+            print(f"L{label:<2}: {U}")
+
+if __name__ == '__main__':
+    test()
